626c2ce74f46b38a44fb2c5d730639343ae7046c
dmakger <dmakger@yandex.ru>
2023-03-20 00:44:38 +0300

    Добавил обновление count_user при добавлении и удалении пользователях у модели "Chat".

--- api/core/admin.py
+++ api/core/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile, Chat, PositionChat, ProfileToChat, Message, Reaction, ReactionToMessage
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+class ChatAdmin(admin.ModelAdmin):
+    pass
+
+
+class PositionChatAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProfileToChatAdmin(admin.ModelAdmin):
+    pass
+
+
+class MessageAdmin(admin.ModelAdmin):
+    pass
+
+
+class ReactionAdmin(admin.ModelAdmin):
+    pass
+
+
+class ReactionToMessageAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Chat, ChatAdmin)
+admin.site.register(PositionChat, PositionChatAdmin)
+admin.site.register(ProfileToChat, ProfileToChatAdmin)
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Reaction, ReactionAdmin)
+admin.site.register(ReactionToMessage, ReactionToMessageAdmin)
--- api/core/models.py
+++ api/core/models.py
@@ -1,13 +1,13 @@
-from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db import models
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 
 # ===={ Юзер }====
 class Profile(models.Model):
     user = models.OneToOneField(to=User, on_delete=models.CASCADE)
-    image = models.ImageField(null=True, default=None)
+    image = models.ImageField(blank=True, null=True, default=None)
 
     def __str__(self):
         return self.user.username
@@ -23,7 +23,7 @@ def create_user(sender, **kwargs):
 # ===={ Чат }====
 class Chat(models.Model):
     title = models.CharField(max_length=64)
-    description = models.TextField(max_length=255, null=True, default=None)
+    description = models.TextField(max_length=255, null=True, default=None, blank=True)
     count_user = models.IntegerField(default=0)
 
     def __str__(self):
@@ -39,16 +39,32 @@ class PositionChat(models.Model):
 
 class ProfileToChat(models.Model):
     chat = models.ForeignKey(to=Chat, on_delete=models.CASCADE)
-    profile = models.ForeignKey(to=Profile, on_delete=models.SET_NULL)
-    position_chat = models.ForeignKey(to=PositionChat, on_delete=models.SET_NULL)
+    profile = models.ForeignKey(to=Profile, on_delete=models.SET_NULL, null=True)
+    position_chat = models.ForeignKey(to=PositionChat, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"{self.pk}) '{self.chat.title}' {self.profile.user.username} [{self.position_chat.title}]"
 
 
+@receiver(post_save, sender=ProfileToChat)
+def add_profile_to_chat(sender, instance, **kwargs):
+    if kwargs['origin']:
+        chat = kwargs['instance'].chat
+        chat.count_user += 1
+        chat.save()
+
+
+@receiver(post_delete, sender=ProfileToChat)
+def pop_profile_to_chat(sender, **kwargs):
+    if kwargs['origin']:
+        chat = kwargs['instance'].chat
+        chat.count_user -= 1
+        chat.save()
+
+
 # ===={ Сообщение }====
 class Message(models.Model):
-    profile_to_chat = models.ForeignKey(to=ProfileToChat, on_delete=models.SET_NULL)
+    profile_to_chat = models.ForeignKey(to=ProfileToChat, on_delete=models.SET_NULL, null=True)
     text = models.TextField()
 
     def __str__(self):
@@ -67,8 +83,7 @@ class Reaction(models.Model):
 class ReactionToMessage(models.Model):
     message = models.ForeignKey(to=Message, on_delete=models.CASCADE)
     profile_to_chat = models.ForeignKey(to=ProfileToChat, on_delete=models.CASCADE)
-    reaction = models.ForeignKey(to=Reaction, on_delete=models.SET_NULL)
+    reaction = models.ForeignKey(to=Reaction, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"{self.pk}) '{self.chat.title}' {self.profile.user.username} [{self.position_chat.title}]"
-
